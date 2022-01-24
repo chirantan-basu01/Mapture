1a1a5d2ba03fb01d9f9012f1dd15e7dc72d829cc
chirantan-basu01 <basuchirantan001@gmail.com>
2022-01-24 17:14:42 +0530

    Initial commit

---
+++ app/src/main/java/bit/basuchirantan/mapture/models/Place.kt
@@ -0,0 +1,3 @@
+package bit.basuchirantan.mapture.models
+
+data class Place(val title: String,val description:String,val latitude: Double,val longitude: Double)
---
+++ app/src/main/java/bit/basuchirantan/mapture/models/UserMap.kt
@@ -0,0 +1,3 @@
+package bit.basuchirantan.mapture.models
+
+data class UserMap(val title: String,val places: List<Place>)
